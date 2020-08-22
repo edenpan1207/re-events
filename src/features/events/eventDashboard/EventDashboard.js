ee9c566d6d2f802798ee5747222706907401078d
y_eden <xadam69@ec-mart.com>
2020-08-22 15:34:19 +0800

    package setup and add features file

---
+++ src/features/events/eventDashboard/EventDashboard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Grid} from 'semantic-ui-react';
+
+const EventDashboard = () => {
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <h2>Left Column</h2>
+      </Grid.Column>
+
+      <Grid.Column width={2}>
+        <h2>Right Column</h2>
+      </Grid.Column>
+    </Grid>
+  )
+}
+
+export default EventDashboard;
